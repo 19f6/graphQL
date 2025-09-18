225fadf551603ca244392b6e686b175162cef32e
19f6 <fatimajassim1266@gmail.com>
2025-09-19 00:34:33 +0300

    replace

--- js/login.js
+++ js/login.js
@@ -1,29 +1,29 @@
-const handleLogin= async () =>{
+const handleLogin = async () => {
     const username = document.getElementById('username').value
     const password = document.getElementById('password').value
     const credential = btoa(`${username}:${password}`)
     try {
-        const response = await fetch('https://learn.reboot01.com/api/auth/signin',{
-               method: 'POST',
-               headers: {
+        const response = await fetch('https://learn.reboot01.com/api/auth/signin', {
+            method: 'POST',
+            headers: {
                 'Authorization': `Basic ${credential}`,
                 'Content-Type': 'application/json'
-
-        }
+            }
         })
-        if(response.ok){
+        if (response.ok) {
             const token = await response.json()
-            sessionStorage.setItem('token',token)
-            window.location.href='profile.html'
+            sessionStorage.setItem('token', token)
+            window.location.replace('profile.html') // Changed from href to replace
         } else {
-            document.getElementById('error-message').textContent='invalid username or password'
+            document.getElementById('error-message').textContent = 'Invalid username or password'
         }
     }
-    catch(error){
-        document.getElementById('error-message').textContent=error
+    catch (error) {
+        document.getElementById('error-message').textContent = error
     }
 }
-document.getElementById('loginForm').addEventListener('submit',function(evennt){
-    evennt.preventDefault()
+
+document.getElementById('loginForm').addEventListener('submit', function(event) { // Fixed typo: evennt -> event
+    event.preventDefault()
     handleLogin()
 })
