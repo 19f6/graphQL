82bb3ae345b79360e9159efed8aee5bc0cbae084
19f6 <fatimajassim1266@gmail.com>
2025-10-03 20:25:09 +0300

    Update level.js

--- js/level.js
+++ js/level.js
@@ -1,13 +1,12 @@
 async function loadLevelProgress() {
   const response = await fetchGraphQL(QUERIES.lEVEL);
-
   const tx = response.data?.transaction || [];
   const level = tx.length > 0 ? tx[0].amount : 0;
 
   const circle = document.querySelector(".level-progress");
   const lv = document.querySelector(".level-text");
 
-  const radius = 40;
+  const radius = parseFloat(circle.getAttribute("r"));
   const circumference = 2 * Math.PI * radius;
   circle.style.strokeDasharray = circumference;
   const progress = (level / 100) * circumference;
